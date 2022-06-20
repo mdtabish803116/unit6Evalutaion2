f758f72ad5113dd257f81b0b7790afd16a4be33e
mdtabish803116 <mdtabish803116@gmail.com>
2022-06-20 22:01:44 +0530

    added new files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import {Link} from "react-router-dom"
+import styled from "styled-components";
+
+const NavbarWrapper = styled.div`
+   display : flex;
+   justify-content : space-around;
+   height : 60px;
+   font-size : 20px;
+   background-color : teal;
+   align-items : center;
+
+`
+const StyledLink = styled(Link)`
+       color : white;
+       font-size : 20px;
+       text-decoration : none;
+`
 
 const Navbar = () => {
-  return <div>{/* CODE HERE */}</div>;
+  return <NavbarWrapper>
+          <StyledLink to="/">Home</StyledLink>
+          <StyledLink to="/login">Login</StyledLink>
+  </NavbarWrapper>;
 };
 
 export default Navbar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,36 @@
 import React, { useState, useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import styled from "styled-components";
 
+const StyledForm = styled.div`
+   width : 50%;
+   margin:auto;
+   margin-top : 80px;
+
+`
+const StyledInput = styled.input`
+        
+  `
+const handleSubmit = (e)=>{
+  e.preventDefault();
+}
 const Login = () => {
-  return <div>{/* CODE HERE */}</div>;
+  const [email , setEmail] = useState("");
+  const [password , setPassword] = useState("");
+
+  return(
+     <StyledForm>
+              
+              <StyledInput type = "email" placeholder="Email" 
+             
+             value = {email} onChange={(e)=>setEmail(e.target.value)}></StyledInput>
+              <br/>
+              <StyledInput type = "password"  placeholder="Password" 
+              value = {password} onChange={(e)=>setPassword(e.target.value)}></StyledInput>
+              <br/>
+              <input type = "submit" value = "Login" onSubmit={handleSubmit}></input>
+    </StyledForm>
+    )
 };
 
 export default Login;
